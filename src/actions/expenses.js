0a4f7976b388d3a69d830f3a017186a228a0c369
slecours <sascha.lecours@gmail.com>
2018-11-11 20:24:48 -0500

    added data fetching from firebase on initial load

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,5 +1,6 @@
 import uuid from 'uuid';
 import database from '../firebase/firebase';
+import expensesFixture from '../tests/fixtures/expenses';
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
@@ -38,3 +39,63 @@ export const editExpense = (id, updates) => ({
 	id,
 	updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+	type: 'SET_EXPENSES',
+	expenses
+});
+
+export const startSetExpenses = () => {
+	return (dispatch) => {
+		return database.ref('expenses')
+			.once('value').then((snapshot) => {
+				const expenses = [];
+				snapshot.forEach((childSnapshot) => {
+					expenses.push({
+						id: childSnapshot.key,
+						...childSnapshot.val(),
+					});
+				});
+				dispatch(setExpenses(expenses));
+			});
+	};
+};
+
+// This code is a copy of what was in "startaddexpenses"
+	// return (dispatch) => {
+	// 	const {
+	// 		description = '',
+	// 		note = '',
+	// 		amount = 0,
+	// 		createdAt = 0
+	// 	} = expenseData;
+	// 	const expense = { description, note, amount, createdAt };
+
+	// 	return database.ref('expenses').push(expense).then((ref) => {
+	// 		dispatch(addExpense({
+	// 			id: ref.key,
+	// 			...expense,
+	// 		}));
+	// 	});
+	// };
+
+// This code is the code Andrew indicated might be useful to parse the fetched info into an array
+	// database.ref('expenses')
+	// 	.on('value', (snapshot) => {
+	// 		const expenses = [];
+
+	// 		snapshot.forEach((childSnapshot) => {
+	// 			expenses.push({
+	// 				id: childSnapshot.key,
+	// 				...childSnapshot.val(),
+	// 			});
+	// 		});
+
+	// 		console.log(expenses);
+	// 	});
+
+
+// 1. Fetch all expense data (from firebase) once
+// 2. Parse that data into an array
+// 3. Dispatch that array using SET_EXPENSES
--- src/app.js
+++ src/app.js
@@ -5,19 +5,23 @@ import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import './firebase/firebase';
-// import './playground/promises';
+
 
 const store = configureStore();
 
 const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+	ReactDOM.render(jsx, document.getElementById('app'));
+});
